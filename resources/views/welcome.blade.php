28f3ee806b3510fc71531a5f8d70edbc68e33567
@kochengaru <sadhanmuriza@gmail.com>
2024-09-08 19:04:16 +0700

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman beranda</title>
+</head>
+<body>
+    <h3>SAYA BELAJAR FRAMEWORK LARAVEL</h3>
+</body>
+</html>
